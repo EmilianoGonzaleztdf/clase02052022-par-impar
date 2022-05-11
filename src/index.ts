28e168b91bc23fc0a67d163a2de63b73ad5eb6c7
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-11 00:05:33 -0300

    funcional la cosa funciona.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-rotulo.innerHTML = "ingrese un numero";
-let dato = document.getElementById("dato");
+rotulo1.innerHTML = "ingrese un numero a verificar";
 
-btnverif.addEventListener("click", () => {
-  let dato = document.getElementById("dato");
-  let dato: number = Number(dato.value);
-  if(dato === 0){
-  console.log("el numero ingresado es cero")
-})
+let numero1 = document.getElementById("dato1");
+
+btn1.addEventListener("click", () => {
+  let numero1: number = Number(dato1.value);
+  if (numero1 == 0) {
+    console.log("El " + numero1 + " no es par ni impar");
+  } else if (numero1 % 2 == 0) {
+    console.log("El " + numero1 + " es par");
+  } else {
+    console.log("El " + numero1 + " es impar");
+  }
+});
